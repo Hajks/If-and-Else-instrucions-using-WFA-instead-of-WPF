5bf95faf34c757809c66b0bcdcdbc32908bb25ff
Hajk <tunczorx@gmail.com>
2018-03-07 21:02:56 +0100

    Add project files.

---
+++ If and Else instrucions using WFA instead of WPF/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace If_and_Else_instrucions_using_WFA_instead_of_WPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
